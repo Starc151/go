848084f46dcd4ce1f53788da042b599f66c03add
AlekseyP <dr.Starc@ya.r>
2022-10-28 16:30:31 +0300

    forInFor end func

--- multiplication/main.go
+++ multiplication/main.go
@@ -3,23 +3,33 @@ package main
 import "fmt"
 import "time"
 
-func main() {
-	c := 0
+func duration(f func()) {
 	start := time.Now()
+	f()
+	duration := time.Since(start)
+	fmt.Println(duration)
+}
+func my() {
 	for i, j := 1, 1; j <= 10; {
 		if j <= i*j && i*j <= j*10 {
 			fmt.Print(i*j, " ")
 			i++
-			c++
 		} else {
 			fmt.Println()
 			j++
 			i = 1
-			c++
 		}
 	}
-	fmt.Println(c)
-	duration := time.Since(start)
-	fmt.Println(duration)
-
+}
+func forInFor() {
+	for i := 1; i <= 10; i++ {
+		for j := 1; j <= 10; j++ {
+			fmt.Print(j, " ")
+		}
+		fmt.Println()
+	}
+}
+func main() {
+	duration(my)
+	duration(forInFor)
 }
